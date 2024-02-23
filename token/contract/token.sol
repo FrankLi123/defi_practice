c5ac2d68deb017ba8b805f35fb6f33a369660d48
FrankLi123 <zoli@ucsd.edu>
2024-02-23 00:28:44 -0500

    implement constructor ( make token capped, send owner initial tokens)

--- token/contract/token.sol
+++ token/contract/token.sol
@@ -1,11 +1,21 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract VibeToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("VibeToken", "VCT" ){
+contract VibeToken is ERC20Capped {
 
-        _mint(msg.sender, initialSupply);
+    address payable public owner;
+
+    // a ** b - a to the power of b
+    constructor(uint256 cap) ERC20("VibeToken", "VCT" ) ERC20Capped(cap * (10 ** decimals())) {
+
+        // set owner to be the user invoking the contract constructor
+        owner = msg.sender;
+
+        // give the creater 7000000 token + 18 decimal points
+        // number of decimal places the token uses
+        _mint(msg.sender, 70000000 * (10 ** decimals() ));
     }
     
 }
