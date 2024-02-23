8182effb0e425cad2b509fe49c664a2b547d92db
FrankLi123 <zoli@ucsd.edu>
2024-02-23 01:38:44 -0500

    fix minor error

--- token/contract/token.sol
+++ token/contract/token.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract VibeToken is ERC20Capped, ERC20Burnable {
 
     address payable public owner;
     uint256 public blockReward;
 
-    // a ** b - a to the power of b
     constructor(uint256 cap, uint256 reward ) ERC20("VibeToken", "VCT" ) ERC20Capped(cap * (10 ** decimals())) {
 
         // set owner to be the user invoking the contract constructor
