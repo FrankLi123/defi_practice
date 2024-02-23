2b39ecc2d76447faaea1106184bc59021233c921
FrankLi123 <zoli@ucsd.edu>
2024-02-23 00:18:13 -0500

    install and use openzeppelin-ERC20contract

--- token/contract/token.sol
+++ token/contract/token.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract VibeToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("VibeToken", "VCT" ){
 
