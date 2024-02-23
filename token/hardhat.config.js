c8eb3006bfe0eb5c98618de651f3fa22b5e96878
FrankLi123 <zoli@ucsd.edu>
2024-02-23 01:38:28 -0500

    configure hardhat compile js.file

--- token/hardhat.config.js
+++ token/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  
+  // any ethereum netowrk to interact with defined here:
+  networks:{
+
+    sepolia: {
+      url: process.env.INFURA_RINKEBY_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
